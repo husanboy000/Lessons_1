cb0235958518b106562664ff4c9c5a2cf1661a62
unknown <husanboyshermuhammedov70@gmail.com>
2025-08-07 09:36:25 +0300

    added 21-lessons

---
+++ lesson_21.py
@@ -0,0 +1,34 @@
+# # # # # # cars={"nomi":"jiguli", "rangi":"qora", "yili":2011}
+# # # # # # rang = (cars["rangi"])
+# # # # # # yili=(cars["yili"])
+# # # # # # print(f"mening mashinamni rangi {rang}, yili {yili}")
+# # # # men={"nem":"Husanboy", "lasnem":"shermuhammedov", "yers":2011, "school":"it", "eag":14}
+# # # # # nem=(men["nem"])
+# # # # # lasnem=(men["lasnem"])
+# # # # # yers=(men["yers"])
+# # # # # school=(men["school"])
+# # # # # ega=(men["eag"])
+# # # # # # print(f"mening ismim {nem}, familyam {lasnem}, tugilgan yilim {yers}, {school}da oqiyman, yoshim {ega}")
+# # # # # # print(men.items())
+# # # # # # for k, q in men.items():
+# # # # #     # print(f"k: {k}")
+# # # # #     # print(f"q: {q}")
+# # # # # print(men.keys())
+# bozorlik=["non", "qaymoq", "sut", "tarvuz", "tuxum", "guruch", "gosht"]
+# mahsulotlar={"non":5000, "qaymoq":50000, "tuxum":2000, "guruch":50000, "gosht":100000, "sut":5000, "tarvuz":3000}
+
+# # # for mahsulot in mahsulotlar:
+# # #     if mahsulot in bozorlik:
+# # #         print(f"{mahsulot.title()}, narx {mahsulotlar [mahsulot]} som")
+# # for buyum in bozorlik:
+# #     if buyum in mahsulotlar:
+# #         print(f"Itimos , dokoningizga {buyum} ham olib keling")
+# print(mahsulotlar.keys())
+# print(mahsulotlar.values())
+
+# for narx in mahsulotlar.values():
+#     print(f"mahsulot narxlar: {narx}")
+
+davlatlar={"O'bekiston":"Toshken", "Rus":"Maskva", "Angila":"London", "Turkiya":"Anqara" "yaponya":"tokyo":}
+for davlat in poytaxt davlatlar:
+    print(f{davlat} davlatlarning poytaxti {poytaxt} shaxarlar)    
